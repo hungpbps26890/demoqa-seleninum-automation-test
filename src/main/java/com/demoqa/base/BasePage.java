49e7b0180b23da6505e5961ace357db576c700a6
hungpbps26890 <hungpbps26890@fpt.edu.vn>
2025-02-22 10:37:50 +0700

    Create BasePage

---
+++ src/main/java/com/demoqa/base/BasePage.java
@@ -0,0 +1,35 @@
+package com.demoqa.base;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class BasePage {
+
+    public static WebDriver driver;
+
+    public void setDriver(WebDriver driver) {
+        BasePage.driver = driver;
+    }
+
+    protected WebElement find(By locator) {
+        return driver.findElement(locator);
+    }
+
+    protected void setText(By locator, String text) {
+        find(locator).clear();
+        find(locator).sendKeys(text);
+    }
+
+    protected void click(By locator) {
+        find(locator).click();
+    }
+
+    protected void delay(int milliseconds) {
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
